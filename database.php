3ce9b56ecec080d09b324792e9f62d280acd0e32
Yash Gandhi <yashg8706@gmail.com>
2020-01-24 14:05:07 -0500

    Error resolving

--- database.php
+++ database.php
@@ -14,7 +14,7 @@ class Database
 	public static function getDb(){
 		if(!isset(self::$dbcon)){
 			try{
-				self::$dbcon = new PDO(self::$dsn, self::$user, self::$pass);
+				self::$dbcon = new PDO($dsn, $user, $pass);
 				self::$dbcon->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
 			} catch (PDOException $e){
 				$msg = $e->getMessage();
