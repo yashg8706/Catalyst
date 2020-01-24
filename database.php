88bca65c21e968331c91c0ae29ed5fb7877ab411
Yash Gandhi <yashg8706@gmail.com>
2020-01-24 14:36:13 -0500

    improvements

--- database.php
+++ database.php
@@ -1,30 +1,29 @@
 <?php
 
+class Database
+{
+	private $url = parse_url(getenv("mysql://b3afd3866c7767:2d3bddce@us-cdbr-iron-east-05.cleardb.net/heroku_dcf0c1cb17b5e2b?reconnect=true"));
 
-$cleardb_url      = parse_url(getenv("mysql://b3afd3866c7767:2d3bddce@us-cdbr-iron-east-05.cleardb.net/heroku_dcf0c1cb17b5e2b?reconnect=true"));
-$cleardb_server   = $cleardb_url["host"];
-$cleardb_username = $cleardb_url["user"];
-$cleardb_password = $cleardb_url["pass"];
-$cleardb_db       = substr($cleardb_url["path"],1);
+	private $server = $url["host"];
+	private $username = $url["user"];
+	private $password = $url["pass"];
+	private $db = substr($url["path"], 1);
 
-class Database {
-	// Static Class DB Connection Variables (for write and read)
-	private static $writeDBConnection;
+	private static $dbcon;
+	private function __construct()
+	{
 
-	// Static Class Method to connect to DB to perform Writes actions
-	// handle the PDOException in the controller class to output a json api error
-	public static function getDb() {
-		if(self::$writeDBConnection === null) {
-
-
-				self::$writeDBConnection = new PDO("mysql:host=".$cleardb_server."; dbname=".$cleardb_db, $cleardb_username, $cleardb_password);
-				self::$writeDBConnection->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
-				self::$writeDBConnection->setAttribute(PDO::ATTR_EMULATE_PREPARES, false);
-
-
-
+	}
+	public static function getDb(){
+		if(!isset(self::$dbcon)){
+			try{
+				self::$dbcon = new mysqli($server, $username, $password, $db);
+			} catch (PDOException $e){
+				$msg = $e->getMessage();
+				echo $msg;
+				exit();
+			}
 		}
-
-		return self::$writeDBConnection;
+		return self::$dbcon;
 	}
 }
