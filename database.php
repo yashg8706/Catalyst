d81c18e9d88656a99545b5e818439554e10fd692
Yash Gandhi <yashg8706@gmail.com>
2020-02-11 01:15:01 -0500

    Update database.php

--- database.php
+++ database.php
@@ -3,12 +3,13 @@
 class Database
 {
 	private $url = parse_url(getenv("mysql://b3afd3866c7767:2d3bddce@us-cdbr-iron-east-05.cleardb.net/heroku_dcf0c1cb17b5e2b?reconnect=true"));
+	//CLEARDB_DATABASE_URL => mysql://[username]:[password]@[host]/[database name]?reconnect=true
 
-	private static $server = $url["host"];
-	private static $username = $url["user"];
-	private static $password = $url["pass"];
+	//private static $server = $url["host"];
+	private static $username = b3afd3866c7767;
+	private static $password = 2d3bddce;
 	private static $db = substr($url["path"], 1);
-	private static $dsn = 'mysql:host='.self::$server.';dbname='.self::$db;
+	private static $dsn = 'mysql:host=us-cdbr-iron-east-05.cleardb.net;dbname=heroku_dcf0c1cb17b5e2b';
 
 	private static $dbcon;
 	private function __construct()
@@ -18,6 +19,7 @@ class Database
 	public static function getDb(){
 		if(!isset(self::$dbcon)){
 			try{
+				var_dump ("inside the getDb"); 
 				self::$dbcon = new PDO(self::$dsn, self::$username, self::$password);
 				self::$dbcon->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
 			} catch (PDOException $e){
