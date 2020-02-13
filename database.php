e280253ba523857b31bcdc00871a75cffe7600a2
Yash Gandhi <yashg8706@gmail.com>
2020-02-13 01:29:49 -0500

    Update database.php

--- database.php
+++ database.php
@@ -5,11 +5,12 @@ class Database
 	private $url = parse_url(getenv("mysql://b3afd3866c7767:2d3bddce@us-cdbr-iron-east-05.cleardb.net/heroku_dcf0c1cb17b5e2b?reconnect=true"));
 	//CLEARDB_DATABASE_URL => mysql://[username]:[password]@[host]/[database name]?reconnect=true
 
-	//private static $server = $url["host"];
-	private static $username = b3afd3866c7767;
-	private static $password = 2d3bddce;
+	$url = parse_url(getenv("mysql://b3afd3866c7767:2d3bddce@us-cdbr-iron-east-05.cleardb.net/heroku_dcf0c1cb17b5e2b?reconnect=true"));
+
+	private static $server = $url["host"];
+	private static $username = $url["user"];
+	private static $password = $url["pass"];
 	private static $db = substr($url["path"], 1);
-	private static $dsn = 'mysql:host=us-cdbr-iron-east-05.cleardb.net;dbname=heroku_dcf0c1cb17b5e2b';
 
 	private static $dbcon;
 	private function __construct()
@@ -20,7 +21,7 @@ class Database
 		if(!isset(self::$dbcon)){
 			try{
 				echo "inside the getDb"; 
-				self::$dbcon = new PDO(self::$dsn, self::$username, self::$password);
+				self::$dbcon = new mysqli(self::$server, self::$username, self::$password, self::$db);
 				self::$dbcon->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
 			} catch (PDOException $e){
 				$msg = $e->getMessage();
@@ -31,3 +32,5 @@ class Database
 		return self::$dbcon;
 	}
 }
+
+
