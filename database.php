d8dd7fa1eaf6b956772d7bef722f30e267b66d82
Yash Gandhi <yashg8706@gmail.com>
2020-02-14 00:30:47 -0500

    Update database.php

--- database.php
+++ database.php
@@ -7,29 +7,24 @@ class Database
 
 	$url = parse_url(getenv("mysql://b3afd3866c7767:2d3bddce@us-cdbr-iron-east-05.cleardb.net/heroku_dcf0c1cb17b5e2b?reconnect=true"));
 
-	private static $server = $url["host"];
-	private static $username = $url["user"];
-	private static $password = $url["pass"];
-	private static $db = substr($url["path"], 1);
+	private $server = $url["host"];
+	private $username = $url["user"];
+	private $password = $url["pass"];
+	private $db = substr($url["path"], 1);
 
-	private static $dbcon;
 	private function __construct()
 	{
 
 	}
-	public static function getDb(){
-		if(!isset(self::$dbcon)){
-			try{
-				echo "inside the getDb"; 
-				self::$dbcon = new mysqli(self::$server, self::$username, self::$password, self::$db);
-				self::$dbcon->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
-			} catch (PDOException $e){
-				$msg = $e->getMessage();
-				echo $msg;
-				exit();
-			}
-		}
-		return self::$dbcon;
+	public function getDb(){
+
+		$dbcon = new mysqli($this->$server, $this->$username, $this->$password, $this->$db);
+		
+		if($dbcon->connect_error)
+		{
+			die("Connection Failed: ".$dbcon->connect_error);
+		}		
+		return $dbcon;
 	}
 }
 
